97e767b1415c6cd7966d4bc33e2046267081d721
Mike Anderson <hello@mrmikea.com>
2014-06-04 21:35:25 -0500

    Render README into gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,54 @@
 var gulp = require("gulp"),
   clean = require("gulp-clean"),
   concat = require("gulp-concat"),
-  deploy = require("gulp-gh-pages");
+  deploy = require("gulp-gh-pages"),
+  markdown = require("gulp-markdown"),
+  merge = require("merge-stream"),
+  removeLines = require("gulp-remove-lines");
 
-gulp.task("bobaJs", function() {
+gulp.task("boba-js", function() {
   return gulp.src("./boba.js")
     .pipe(gulp.dest("./gh-pages"));
 });
 
-gulp.task("bobaBrowserifyJs", function() {
+gulp.task("boba-browserify-js", function() {
   return gulp.src(["./boba.js", "./browserify-export.js"])
     .pipe(concat("boba-browserify.js"))
     .pipe(gulp.dest("./gh-pages"));
 });
 
 gulp.task("clean", function() {
-  return gulp.src("./gh-pages", {read: false})
-    .pipe(clean());
+  return merge(
+    gulp.src("./gh-pages/**/*", {read: false})
+      .pipe(clean()),
+    gulp.src("./tmp", {read: false})
+      .pipe(clean())
+  );
 });
 
-gulp.task("ghPages", function() {
+gulp.task("gh-pages-readme", function() {
+  return gulp.src("./README.md")
+    .pipe(markdown())
+    .pipe(removeLines({filters: [
+      /id="boba-js"/,
+      /id="contributing"/,
+      /the contributing guide/
+    ]}))
+    .pipe(gulp.dest("./tmp"));
+});
+
+gulp.task("gh-pages-index", ["gh-pages-readme"], function() {
+  return gulp.src(["./gh-pages-src/top.html", "./tmp/README.html", "./gh-pages-src/bottom.html"])
+    .pipe(concat("index.html"))
+    .pipe(gulp.dest("./gh-pages"));
+});
+
+gulp.task("build-js", ["boba-js", "boba-browserify-js"]);
+
+gulp.task("default", ["clean", "build-js", "gh-pages-index"]);
+
+gulp.task("gh-pages", ["default"], function() {
   return gulp.src("./gh-pages/**/*")
     .pipe(deploy());
 });
-
-gulp.task("buildJs", ["bobaJs", "bobaBrowserifyJs"]);
-
-gulp.task("default", ["clean", "buildJs"]);
 
